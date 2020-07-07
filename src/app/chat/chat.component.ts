55f3c196333c4f0925185c717dd39b2e4a21dc89
Mathew Daniel <mathewdaniel@Mathews-MacBook-Pro.local>
2020-07-07 09:41:29 -0500

    .

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -18,37 +18,69 @@ export class ChatComponent implements OnInit {
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
+
+    document.querySelector('textarea').addEventListener("keypress",(e)=>{
+      if (e.keyCode == 13 && !e.shiftKey) this.sendMsg();
+    });
+
     this.room = this.auth.roomData;
-    this.socket = io("http://localhost:5000/chat");
+    this.socket = io("http://localhost:5000/chat",{forceNew:true});
     this.socket.emit("join",{id:this.auth.roomData.socket_Room,password:this.auth.roomData.password,username:this.auth.username});
 
-    this.socket.on("updateLog",(msg)=>{
+    this.socket.on("updateLog",(msg) => {
+
+      if (this.messages.length > 1000) {
+        this.messages = this.messages.slice(0,501);
+      }
+
       this.messages.push({sender:msg.sender,txt:msg.updateLog,date:new Date(msg.date)});
+
+      setTimeout(()=>{
+        let msgs = document.querySelector('.msgs');
+        if ((msgs.scrollHeight - msgs.scrollTop - msgs.clientHeight) < 50) {
+          msgs.scrollTop = 1000000;
+        }
+      },500);
+
     });
 
     this.socket.on("newUser",(user) => {
-      this.users.push(user);
+      if (this.users.indexOf(user) == -1) {
+        this.users.push(user);
+      }
       if (user != this.auth.username) {
         this.socket.emit("sendToNewUser",{username:this.auth.username});
       }
     });
 
-    this.socket.on("addUser",(user)=>{
+    this.socket.on("addUser",(user) => {
       if (this.users.indexOf(user) == -1) {
         this.users.push(user);
       }
     });
 
-    this.socket.on("userLeft",(user)=>{
-      console.log(`${user} left`);
+    this.socket.on("userLeft",(user) => {
       this.users.splice(this.users.indexOf(user),1);
     });
 
   }
 
   sendMsg() {
-    this.socket.emit("msg",{txt:this.text,sender:this.auth.username});
-    this.text = "";
+    const re = new RegExp('^\\s*$');
+    if (this.text.length >= 1 && !re.test(this.text)) {
+
+      if (this.socket.connected) {
+        this.socket.emit("msg",{txt:this.text,sender:this.auth.username});
+      } else {
+        this.socket = io("http://localhost:5000/chat",{forceNew:true});
+        this.socket.emit("join",{id:this.auth.roomData.socket_Room,password:this.auth.roomData.password,username:this.auth.username});
+      }
+      this.text = "";
+
+    }
+
+
+
   }
 
 }
